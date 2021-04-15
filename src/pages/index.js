05e5f9302f9b4eedf812abae78737f8c0f35d933
Aris Riswanto <aris@mail.kliksob.com>
2021-04-16 00:26:21 +0700

    Test Deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Layout from "@/components/Layout";
 export default function PageHome(props) {
   return (
-    <Layout withHero={{title: "Hi", subtitle: "Sub"}}>
+    <Layout withHero={{title: "Hero Title", subtitle: "Hero Subtitle"}}>
       <Head>
         <title>Home Page</title>
       </Head>
